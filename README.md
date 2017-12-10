2e212103dc50bfdfc12b63c8a51bc90a2f69f1c6
nar789 <tony92511@hanmail.net>
2017-12-10 16:05:23 +0900

    update str

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 171125 1st complete.
 171126 db backup
 171208 app copy 5 finish.
+171210 update str
 ```
--- strweb/r.php
+++ strweb/r.php
@@ -8,7 +8,15 @@
 <body>
 	<div class=find onclick="connect()"><img src=img/find.png class=findimg></div>
 	<div class=chatbox></div>
-	<!--<div class=ad></div>//-->
+	<div class=ad><?
+		include("dblib.php");
+		$conn=mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
+		if(!$conn)echo "error!";
+		$ret=mysqli_query($conn,"select * from ad");
+		$r=mysqli_fetch_array($ret);
+		echo "<img onclick='adclick(\"".$r['url']."\")' class=adimg src=".$r['img'].">";
+		mysqli_close($conn);
+	?></div>
 	<div class=chatinput>
 		<div class=new-g><div class=blank></div><img src=img/new.png onclick="connect()" class=new></div><input type=text id=msg class=inputtext placeholder="메세지를 입력하세요."><img src=img/btn_chat.png class=btn onclick='send()'>
 	</div>
